413162e11d4bca44c738975eefeb10c368fa6b0d
sukruthganesh <ganeshsukruth@gmail.com>
2024-02-15 22:09:09 -0600

    Solution to Exercise 5

--- app.py
+++ app.py
@@ -74,6 +74,13 @@ def render_with_error_handling(template, **kwargs):
     except:
         t = traceback.format_exc()
         return render_template('error.html', args={"trace": t}), 500
+    
+def validate_api_key(req):
+    user_id = req.headers['User-Id']
+    api_key = req.headers['Api-Key']
+    if user_id and api_key:
+        return query_db('select * from users where id = ? and api_key = ?', [user_id, api_key], one=True)
+    return None
 
 # ------------------------------ NORMAL PAGE ROUTES ----------------------------------
 
@@ -161,6 +168,9 @@ def logout():
     resp.set_cookie('user_password', '')
     return resp
 
+def get_last_msg_id():
+    return query_db('select max(id) from messages', [], one=True)
+
 @app.route('/rooms/<int:room_id>')
 def room(room_id):
     user = get_user_from_cookie(request)
@@ -173,14 +183,86 @@ def room(room_id):
 # -------------------------------- API ROUTES ----------------------------------
 
 # POST to change the user's name
-@app.route('/api/user/name')
-def update_username():
-    return {}, 403
+@app.route('/api/user/name', methods=['POST'])
+def change_username():
+    user = validate_api_key(request)
+    if user:
+        if request.method == 'POST':
+            update_user = query_db('update users set name = ? where api_key = ?', 
+                [request.args['user_name'], request.headers['Api-Key']],
+                one=True
+            )
+            response = make_response(redirect('/profile'))
+            response.set_cookie('user_id', request.args['user_name'])
+            return {}
+    else:
+        return render_template('error.html', args={"trace: ": 'Invalid User Credentials'}), 401
+    return {'Status': 'Something went wrong!!'}, 403
 
 # POST to change the user's password
+@app.route('/api/user/password', methods=['POST'])
+def change_password():
+    user = validate_api_key(request)
+    if user:
+        if request.method == 'POST':
+            update_password = query_db('update users set password = ? where api_key = ?',
+                [request.headers['Password'], request.headers['Api-Key']],
+                one=True
+            )
+            password = request.headers['Password']
+            resp = make_response({'pass':'Password set successfully!'})
+            resp.set_cookie('user_password', password)
+            return resp
+    else:
+        return render_template('error.html', args={"trace: ": 'Invalid User Credentials'}), 401
+    return {'Status': 'Something went wrong!!'}, 403
+
 
 # POST to change the name of a room
+@app.route('/api/room/name', methods=['POST'])
+def change_room():
+    user = validate_api_key(request)
+    if user:
+        if request.method == 'POST':
+            update_room = query_db('update rooms set name = ? where id = ?',
+                [request.args['name'], request.args['room_id']],
+                one=True
+            )
+            return {'status': 'Success'}, 200
+    else:
+        return render_template('error.html', args={"trace: ": 'Invalid User Credentials'}), 401
+    return {'Status': 'Something went wrong!!'}, 403
 
 # GET to get all the messages in a room
+@app.route('/api/room/messages', methods=['GET'])
+def get_chat_messages():
+    output = {}
+    user = validate_api_key(request)
+    if user:
+        if request.method == 'GET':
+            room_id = request.args['room_id']
+            messages = query_db('select msg.id, u.name, msg.body from messages msg, users u '
+                        'where msg.room_id = ? and msg.user_id = u.id order by msg.id', [room_id], one=False)
+            if not messages:
+                return output
+            for msg in messages:
+                output[msg[0]] = {'id': msg[0], 'name': msg[1], 'body': msg[2]}
+        return output, 200
+    else:
+        return render_template('error.html', args={"trace: ": 'Invalid User Credentials'}), 401
+    return {'Status': 'Something went wrong!!'}, 403
+
 
 # POST to post a new message to a room
+@app.route('/api/room/new_msg', methods=['POST'])
+def post_message():
+    user = validate_api_key(request)
+    if user:
+        last_msg_id = get_last_msg_id()
+        if request.method == 'POST':
+            insert_messages = query_db('insert into messages (id, user_id, room_id, body) ' + 'values (?, ?, ?, ?)',
+                (int(last_msg_id[0]) + 1, request.headers['User-Id'], request.args['room_id'], request.args['body']), one=True)
+            return {'status': 'Success'}, 200
+    else:
+        return render_template('error.html', args={"trace: ": 'Invalid User Credentials'}), 401
+    return {'Status': 'Something went wrong!!'}, 403
--- static/script.js
+++ static/script.js
@@ -4,20 +4,127 @@
 // POST to the API when the user posts a new message.
 // Automatically poll for new messages on a regular interval.
 // Allow changing the name of a room
-function postMessage() {
+const api_key = WATCH_PARTY_API_KEY;  
+const user_id = WATCH_PARTY_USER_ID;
+const ALL_MESSAGES = '/api/room/messages';
+const POST_MESSAGE = '/api/room/new_msg';
+const CHANGE_USERNAME = '/api/user/name';
+const CHANGE_PASSWORD = '/api/user/password';
+const CHANGE_ROOM = '/api/room/name';
+let max_id = 0;
+
+let getAllChatsReq = {
+  room_id: 0
+};
+
+let postRequest = {
+  room_id: 0,
+  body: ''
+};
+
+let updateUserNameRequest = {
+  user_name: ''
+};
+
+let updatePasswordRequest = {
+  password: ''
+};
+
+let updateRoomRequest = {
+  name: '',
+  room_id: 0
+};
+
+async function generateUrl(endPoint, requestBody, type){
+  let url = endPoint + "?" + Object.keys(requestBody).map((key) => key+"="+encodeURIComponent(requestBody[key])).join("&");
+  let urlHeaders = new Headers();
+  urlHeaders.append("Api-Key", api_key);
+  urlHeaders.append("Accept", "application/json");
+  urlHeaders.append("Content-Type", "application/json");
+  urlHeaders.append("User-Id", user_id);
+
+  if(endPoint == CHANGE_PASSWORD){
+    urlHeaders.append("Password", Object.keys(requestBody).map((key) => requestBody[key]).join(''));
+    url = endPoint;
+  }
+
+  const dict = {
+    method: type,
+    headers: urlHeaders,
+  };
+
+  data = await fetch(url, dict);
+  jsonForm = await data.json();
+  return jsonForm
+}
+
+async function handleChangeUserName() {
+  updateUserNameRequest.user_name = document.getElementById('usrName').value;
+  postMsg = await generateUrl(CHANGE_USERNAME, updateUserNameRequest, 'POST');
+  return;
+} 
+
+async function handleUpdatePassword() {
+  updatePasswordRequest.password = document.getElementById('pass').value;
+  postMsg = await generateUrl(CHANGE_PASSWORD, updatePasswordRequest, 'POST');
   return;
 }
 
-function getMessages() {
+async function postMessage(room_id) {
+  postRequest.room_id = room_id;
+  postRequest.body = document.querySelector('.comment_box textarea').value;
+  postMsg = await generateUrl(POST_MESSAGE, postRequest, 'POST');
+  document.querySelector(".comment_box form").reset();
   return;
 }
 
-function startMessagePolling() {
+async function getMessages(room_id) {
+  getAllChatsReq.room_id = room_id;
+  let msgs = await generateUrl(ALL_MESSAGES, getAllChatsReq, 'GET')
+  let msgsDiv = document.body.querySelector(".messages");
+  let child = msgsDiv.lastElementChild;
+  while (child) {
+    msgsDiv.removeChild(child);
+    child = msgsDiv.lastElementChild;
+  }
+
+  Object.keys(msgs).forEach(key => {
+    let msg = document.createElement("message");
+    let author = document.createElement("author");
+    author.innerHTML = msgs[key].name;
+    let content = document.createElement("content");
+    content.innerHTML = msgs[key].body;
+    msg.appendChild(author);
+    msg.appendChild(content);
+    msgsDiv.append(msg);
+  });
   return;
 }
 
-function updateRoomName() {
+async function startMessagePolling(room_id) {
+  setInterval(async () => {
+    await getMessages(room_id);
+  }, 100);
   return;
+}
+
+async function updateRoomName(room_id) {
+  newRoomName = document.querySelector('.edit input').value;
+  updateRoomRequest.name = newRoomName;
+  updateRoomRequest.room_id = room_id;
+  let resp = await generateUrl(CHANGE_ROOM, updateRoomRequest, 'POST');
+  edit_h3 = document.querySelector(".display");
+  edit_h3.classList.remove("hide");
+  edit_input = document.querySelector(".edit");
+  edit_input.classList.add("hide");
+  document.querySelector('.display .roomName').innerHTML = updateRoomRequest.name;
+}
+
+function handleClickEditRoom() {
+  edit_input = document.querySelector(".edit");
+  edit_input.classList.remove("hide");
+  edit_h3 = document.querySelector(".display");
+  edit_h3.classList.add("hide");
 }
 
 /* For profile.html */
